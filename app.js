525481fac486a3f6379f0c0e1a5afe8365eebaf6
Dev Kumar Singh <gohi772004@gmail.com>
2025-06-13 14:29:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
+
+module.exports = app;
